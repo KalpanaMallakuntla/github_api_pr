3c4e97bf9cd9209f8554d7ab7d6606bb54410c9e
KalpanaMallakuntla <93181810+KalpanaMallakuntla@users.noreply.github.com>
2022-02-22 18:59:00 +0530

    Update pullrequests.sh
    Updated script removed commented out lines

--- pullrequests.sh
+++ pullrequests.sh
@@ -1,15 +1,12 @@
 #!/usr/bin/bash
+echo -n "enter project name: "
+read project
 echo -n "enter repo name: "
 read repo
-curl -H "Accept: application/json" https://api.github.com/repos/KalpanaMallakuntla/$repo/pulls | jq -r '.[] | [.id, .state, .draft, .label, .created_at] | @csv' > pullrequests.txt
-curl -H "Accept: application/json" https://api.github.com/repos/KalpanaMallakuntla/$repo/pulls?state=closed | jq -r '.[] | [.id, .state, .draft, .label, .created_at] | @csv' >> pullrequests.txt
+curl -H "Accept: application/json" https://api.github.com/repos/$project/$repo/pulls | jq -r '.[] | [.id, .state, .draft, .label, .created_at] | @csv' > pullrequests.txt
+curl -H "Accept: application/json" https://api.github.com/repos/$project/$repo/pulls?state=closed | jq -r '.[] | [.id, .state, .draft, .label, .created_at] | @csv' >> pullrequests.txt
 cat pullrequests.txt | sed 's/"//g' | cut -dT -f1 | sed 's/-//g' | sed 's/,/ /g' > /tmp/explore.txt
-  #    exit 0
-# else
- #     echo "not the expected output"
- #fi
-  #done < /tmp/created_at.txt
-#done
+#enter date
 echo -n "enter end date: "
 read enddate
 while read line; do
